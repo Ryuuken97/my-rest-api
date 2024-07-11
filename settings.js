8750e29522dea0dd0abf3321b69f5a2140adb085
Ryuuken <133407503+Ryuuken97@users.noreply.github.com>
2024-07-11 21:18:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb://IDX_medicineso:83a99558f54d5b5b6c0036b1066b08403d3fa925@gnv.h.filess.io:27018/IDX_medicineso" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "ryuukencifer@gmail.com" //email
+global.email_password = "nzwm tzvx jgeo phkx" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
